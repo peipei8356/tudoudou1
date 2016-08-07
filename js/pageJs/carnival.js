8401625f559e19c78680a11121e444d8dcc30862
crazy <Crazy@MacdeMacBook-Pro.local>
2016-08-07 15:22:54 +0800

    ..

--- js/pageJs/carnival.js
+++ js/pageJs/carnival.js
@@ -2,16 +2,26 @@
 $(function (){
 
     //banner切换
-    var swiper3 = new Swiper('.swiper-container-banner', {
+    var swiper = new Swiper('.swiper-container-banner', {
         pagination: '.swiper-pagination-banner',
         paginationClickable: true,
         autoplay: 9000
     });
 
-    var swiper2 = new Swiper('.swiper-container-active', {
-        nextButton: '.swiper-button-next-active',
-        prevButton: '.swiper-button-prev-active',
-        pagination: '.swiper-pagination-active',
-        paginationType: 'fraction'
-    });
+    <!--活动花絮-->
+    var mySwiper = new Swiper('.active-swiper-con',{
+        pagination: '.active-pagination',
+        loop:true,
+        grabCursor: true,
+        paginationClickable: true
+    })
+    $('.arrow-left').on('click', function(e){
+        e.preventDefault()
+        mySwiper.swipePrev()
+    })
+    $('.arrow-right').on('click', function(e){
+        e.preventDefault()
+        mySwiper.swipeNext()
+    })
+
 });
