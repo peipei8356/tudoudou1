0adddeb9f08c12e952cf5c984d4d99a1aca4b0e6
Ava_kong <peipei8356@163.com>
2016-08-08 01:26:07 +0800

    commit new-files 2016/8/8

--- js/appComm.js
+++ js/appComm.js
@@ -3,26 +3,39 @@ var appComm = {};
 appComm.chartArr = [];
 appComm.init = function () {
     var pageJs = $("#currPage").attr("data-pageJs");
+    appComm.appVersion();
     if (typeof (pageJs) != 'undefined') {
         require([pageJs], function (page) {
             page.init();
         });
     }
 };
-var wait = 60;
+appComm.wait = 60;
 //60秒后重新获取验证码
 appComm.getGetValidateCode60=function(btn){
-    if (wait===0) {
+    if (appComm.wait===0) {
         btn.removeAttribute("disabled");
         btn.value = "获取验证码";
-        wait = 60;
+        appComm.wait = 60;
     }else{
         btn.setAttribute("disabled",true);
-        btn.value = wait + "秒后重试";
-        wait--;
+        btn.value = appComm.wait + "秒后重试";
+        appComm.wait--;
         setTimeout(function(){
             appComm.getGetValidateCode60(btn);
         },1000);
+    }
+};
+appComm.appVersion=function(){
+    var browser=navigator.appName
+    var b_version=navigator.appVersion
+    var version=b_version.split(";");
+    var trim_Version=version[1].replace(/[ ]/g,"");
+   if(browser=="Microsoft Internet Explorer" && trim_Version=="MSIE8.0")
+    {
+        //placehodler兼容IE8
+        require([placehodler],function(){
+        });
     }
 };
 
--- js/pageJs/register.js
+++ js/pageJs/register.js
@@ -1,13 +1,18 @@
 /**
  * Created by Admin on 2016/8/5.
  */
-define(["pageJs/account-security"],function(acPage){
+define([],function(){
     var page={};
     page.init=function(){
         $(".tabcon-li-02 li").on("click",function(){
             $(this).siblings("li").removeClass("on");
             $(this).addClass("on");
         });
+        //验证码
+        $("#send-mobile-code").click(function(){
+            appComm.getGetValidateCode60(this);
+        });
+
 
     };
     return page;
