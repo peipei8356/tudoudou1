af9c7b835c8931974368414d26db03425e8dd62d
Ava_kong <peipei8356@163.com>
2016-08-07 15:58:51 +0800

    change index.html

--- js/pageJs/index.js
+++ js/pageJs/index.js
@@ -1,60 +1,39 @@
 
-/*
 $(function (){
-
-<<<<<<< HEAD
-=======
     //banner切换
-    var swiper = new Swiper('.swiper-container-banner', {
-        pagination: '.swiper-pagination-banner',
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
         paginationClickable: true,
         autoplay: 9000
     });
->>>>>>> 8401625f559e19c78680a11121e444d8dcc30862
 
+    //tab切换
+    $('.tabcon li').plusTab({
+        opt_2: '.wrapcon'
+    });
 
-});
-*/
+    //动画
+    $(window).scroll(function(){
+        var scrollTop = $(document).scrollTop();
+        if( scrollTop <= 550 ){
+            $('.fun').removeClass('active');
+        }
+        if( scrollTop > 550 && scrollTop <= 800){
+            $('.fun').removeClass('active');
+            $('.fun1').addClass('active');
+        }
+        if( scrollTop > 1100 && scrollTop <= 1500){
+            $('.fun').removeClass('active');
+            $('.fun2').addClass('active');
+        }
+        if( scrollTop > 1600 && scrollTop <= 2550){
+            $('.fun').removeClass('active');
+            $('.fun3').addClass('active');
+        }
+        if( scrollTop > 2550){
+            $('.fun').removeClass('active');
+            $('.fun4').addClass('active');
+        }
+    });
 
-define([require],function(require){
-   /* var jwplayer=require("jwplayer");*/
-    var page={};
-    page.init=function(){
-        //banner切换
-        var swiper = new Swiper('.swiper-container', {
-            pagination: '.swiper-pagination',
-            paginationClickable: true,
-            autoplay: 9000
-        });
-
-        //tab切换
-        $('.tabcon li').plusTab({
-            opt_2: '.wrapcon'
-        });
-
-        //动画
-        $(window).scroll(function(){
-            var scrollTop = $(document).scrollTop();
-            if( scrollTop <= 550 ){
-                $('.fun').removeClass('active');
-            }
-            if( scrollTop > 550 && scrollTop <= 800){
-                $('.fun').removeClass('active');
-                $('.fun1').addClass('active');
-            }
-            if( scrollTop > 1100 && scrollTop <= 1500){
-                $('.fun').removeClass('active');
-                $('.fun2').addClass('active');
-            }
-            if( scrollTop > 1600 && scrollTop <= 2550){
-                $('.fun').removeClass('active');
-                $('.fun3').addClass('active');
-            }
-            if( scrollTop > 2550){
-                $('.fun').removeClass('active');
-                $('.fun4').addClass('active');
-            }
-        });
-    };
-    return page;
 });
